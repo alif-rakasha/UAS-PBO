b5ac4ba0f2b3cb4d29ce97032cf55a18899ee112
alif-rakasha <85238085+alif-rakasha@users.noreply.github.com>
2021-07-07 14:59:38 +0700

    Add files via upload

---
+++ UAS_PBO/src/packagePBO/config.java
@@ -0,0 +1,22 @@
+package packagePBO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class config {
+    private static Connection MySQLConfig;
+    public static Connection configDB()throws SQLException{
+        try {
+            String url = "jdbc:mysql://localhost:3306/databasetoko";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+        }catch(SQLException e){
+            System.out.println("Koneksi ke Database Gagal. " + e.getMessage());
+        }
+        return MySQLConfig;
+    }
+}
